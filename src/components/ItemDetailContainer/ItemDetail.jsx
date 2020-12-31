37d4b971a1c6ddd98a850f131f86e353013063dc
Maximo <maximo1611@gmail.com>
2020-12-31 00:39:25 -0300

    detalle de producto inicio

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -1,18 +1,26 @@
 import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
-import ProductDetail from './ItemDetailConteiner';
+import Instrumentos from '.././ItemListContainer/ProductList/productlist'
  const Detail = () => {
     const {id} = useParams();
     const [product, setProduct] = useState([]);
+   
+
+   
+
     const getProduct = new Promise((resolve, reject) => {
+        let lista = Instrumentos.filter(detalle => detalle.id === id)
+        console.log(lista)
         setTimeout(() => {
-            resolve({
+            resolve( {
                 id: id, 
-                nombre: "Producto de prueba",
-                foto: "http://placehold.it/350x400",
-                descripcion: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam laboriosam deleniti neque! Explicabo aspernatur accusantium ex provident natus, nam neque nesciunt eaque iure dolore, architecto maiores corrupti deserunt totam. Veniam.",
-                precio: 400
-            })
+                Marca: lista.Marca,
+                Imagen: lista.Imagen,
+                Modelo: lista.Modelo,
+                descripcion: lista.descripcion,
+                Precio: lista.precio,
+                Quantity: lista.Quantity
+            } )
         }, 500);
     });
     useEffect(() => {
@@ -26,12 +34,13 @@ import ProductDetail from './ItemDetailConteiner';
         <>
             { product ?
                 <div className="container">
-                    <ProductDetail item={product} />
                     <section>
-                        El id del producto seleccionado es: {id}
+                      <p>  El id del producto seleccionado es: {id}</p>
+                      <div></div>
+                    
                     </section>
                 </div> : 
-                <p>Cargando producto...</p>  }
+                <p>Cargando producto...</p> }
         </>
     )
 }
--- src/components/ItemDetailContainer/ItemDetailConteiner.jsx
+++
@@ -1,3 +0,0 @@
-export default function ProductDetail(){
-    return( <h1>hOLA</h1>)
-}
