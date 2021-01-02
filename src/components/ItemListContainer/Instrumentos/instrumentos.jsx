a53f70027c51643df9bc14243081621ae9530fd5
Maximo <maximo1611@gmail.com>
2021-01-02 13:31:55 -0300

    prueba de carga

--- src/components/ItemListContainer/Instrumentos/instrumentos.jsx
+++ src/components/ItemListContainer/Instrumentos/instrumentos.jsx
@@ -6,33 +6,31 @@ import Instrumentos from '../ProductList/productlist';
 
 function Instrumento() {
 
-     const [items, setItems] = useState([0]);
+     const [items, setItems] = useState([]);
      const { instrumentos } = useParams() ;
-
-    useEffect(() => {
+     
+         useEffect(() => {
               getProducstFromDB();
+              setItems([])
               // eslint-disable-next-line react-hooks/exhaustive-deps
        }, [instrumentos])
 
       const lista = Instrumentos.filter(e => e.categoria === instrumentos)
-
        const getProducts = new Promise((resolve, reject) => {
                      resolve(lista);
        })
        const getProducstFromDB = async () => {
               try {
                      const result = await getProducts;
-                
                      setItems(result);
-                    
               } catch (error) {
                      alert('No podemos mostrar los productos en este momento');
               }
        }
 
-       return (<section className='contenedor_instrumentos'>{ lista.length > 0 ? <>
+       return (<section className='contenedor_instrumentos'>{ items.length ? <>
               {(items.map(u =>
-              
+               
                      <Link to={`/items/${u.id}`} className='VerMas'>
                             <div className='Producto' key={u.id}>
                                    <p className='txt_producto'>{u.Marca} {u.Modelo}</p>
