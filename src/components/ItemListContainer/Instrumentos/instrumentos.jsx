105b43533f1e3eea85f348853dbd4cdf9b64c183
Maximo <maximo1611@gmail.com>
2021-01-12 01:27:12 -0300

    Cart pudiendo eliminar productos sin bugs

--- src/components/ItemListContainer/Instrumentos/instrumentos.jsx
+++ src/components/ItemListContainer/Instrumentos/instrumentos.jsx
@@ -1,49 +1,58 @@
-import '../../ItemListContainer/style_Instrumentos.css';
-import Loading from '../../assets/loading.gif';
-import React, { useState, useEffect } from 'react';
-import { Link, useParams} from 'react-router-dom';
-import Instrumentos from '../ProductList/productlist';
+import "../../ItemListContainer/style_Instrumentos.css";
+import Loading from "../../assets/loading.gif";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import Instrumentos from "../ProductList/productlist";
 
 function Instrumento() {
+  const [items, setItems] = useState([]);
+  const { instrumentos } = useParams();
 
-     const [items, setItems] = useState([]);
-     const { instrumentos } = useParams() ;
-     
-         useEffect(() => {
-              getProducstFromDB();
-              // eslint-disable-next-line react-hooks/exhaustive-deps
-       }, [instrumentos])
+  useEffect(() => {
+    getProducstFromDB();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [instrumentos]);
 
-      const lista = Instrumentos.filter(e => e.categoria === instrumentos)
-       const getProducts = new Promise((resolve, reject) => {
-                     resolve(lista);
-       })
-       const getProducstFromDB = async () => {
-              try {
-                     const result = await getProducts;
-                     setItems(result);
-              } catch (error) {
-                     alert('No podemos mostrar los productos en este momento');
-              }
-       }
+  const lista = Instrumentos.filter((e) => e.categoria === instrumentos);
+  const getProducts = new Promise((resolve, reject) => {
+    resolve(lista);
+  });
+  const getProducstFromDB = async () => {
+    try {
+      const result = await getProducts;
+      setItems(result);
+    } catch (error) {
+      alert("No podemos mostrar los productos en este momento");
+    }
+  };
 
-       return (<section className='contenedor_instrumentos'>{ items.length ? <>
-              {(items.map(u =>
-               
-                     <Link to={`/items/${u.id}`} className='VerMas'>
-                            <div className='Producto' key={u.id}>
-                                   <p className='txt_producto'>{u.Marca} {u.Modelo}</p>
-                                   <img src={u.Imagen} alt="Guitarra" className="Foto_instrumento" />
-                                   <p className='txt_producto'>{u.Precio}</p>
-                            </div>
-                    </Link>
-
-              ))}
-       </> : <div>
-                     <img src={Loading} alt="Cargando..." />
-                     <p className='txt_cargando'>Cargando</p>
+  return (
+    <section className="contenedor_instrumentos">
+      {items.length ? (
+        <>
+          {items.map((u) => (
+            <Link to={`/items/${u.id}`} className="VerMas">
+              <div className="Producto" key={u.id}>
+                <p className="txt_producto">
+                  {u.Marca} {u.Modelo}
+                </p>
+                <img
+                  src={u.Imagen}
+                  alt="Guitarra"
+                  className="Foto_instrumento"
+                />
+                <p className="txt_producto">{u.Precio}</p>
               </div>
-       }</section>
-       )
+            </Link>
+          ))}
+        </>
+      ) : (
+        <div>
+          <img src={Loading} alt="Cargando..." />
+          <p className="txt_cargando">Cargando</p>
+        </div>
+      )}
+    </section>
+  );
 }
-export default Instrumento
+export default Instrumento;
--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -5,32 +5,39 @@ import Loading from "../assets/loading.gif";
 
 const Cart = () => {
 
-    // eslint-disable-next-line no-unused-vars
     const [data, setData] = useContext(Store);
     const copyData = data;
-    // console.log(JSON.stringify(copyData));
-   
-
+    // eslint-disable-next-line no-self-assign
+    
     
 function deleteItem (id){
  let lista_filtrada =  copyData.items.filter(e => e.id === id);
-copyData.items.shift(lista_filtrada)
+ console.log()
+ var idx = copyData.items.indexOf(lista_filtrada[0]);
+ console.log(idx)
+copyData.items.splice(idx , 1)
 setData({
     ...copyData, 
    cantidad: data.items.reduce((acc, i)=> acc + i.cantidad, 0)})
 localStorage.setItem("Cart", JSON.stringify({
     ...data, 
-    id: id , 
+    id: id, 
     cantidad: data.items.reduce((acc, i)=> acc + i.cantidad, 0)})
   )
    
 }
 
-    return (
-        <>?<h1>Estás en el carrito</h1>
+    return ( <div>{
+    copyData ? 
+         <>  
+        <h1>Estás en el carrito</h1>
             <h2>Cantidad total: {data.cantidad}</h2>
           {data.items.map(i => <h3>{i.product} - {i.cantidad} <ImCross onClick={()=> {deleteItem(i.id)}} /> </h3>)}
-        </>
+        </> :            <div>
+              <img src={Loading} alt="Cargando..." />
+              <p className="txt_cargando">Cargando</p>
+            </div>
+        } </div>
     )
 }
 
