6a4cf9ecb78b8b8857c9d27ac4df6935eabc07da
Max <64936675+maximo1611@users.noreply.github.com>
2021-01-13 16:39:49 -0300

    Sin Repetición

---
+++ components/ItemDetailContainer/ItemDetail.jsx
@@ -0,0 +1,112 @@
+import "../ItemDetailContainer/itemDetailStyle.css";
+import { useContext, useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import Instrumentos from ".././ItemListContainer/ProductList/productlist";
+import Counter from "../Counter/counter";
+import Loading from "../assets/loading.gif";
+import { Store } from "../../store/index";
+
+const Detail = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState([]);
+  const [data, setData] = useContext(Store);
+  const history = useHistory();
+
+  function onAdd(count, product) {
+
+    if(data.items.length > 0){
+    var itemInCart = data.items.some(i => i.id === id);
+    }else{
+     itemInCart = false
+    }
+    if(itemInCart){
+      const newItems = [...data.items];
+      newItems.forEach((i)=>{
+        if(i.id === id){
+          i.cantidad+= count;
+        }
+      })
+      setData({
+        items: newItems,
+        cantidad: JSON.stringify(newItems.reduce((acc, item)=> acc+=item.cantidad)),
+        precioTotal: 0
+      })
+      console.log(data);
+  } else {
+     //si llegaste aca es porque el producto no se encuentra en el cart, por lo que podes agregarlo normalmente
+  
+    setData({
+      ...data, 
+      id: id ,     
+      cantidad: JSON.stringify( data.cantidad + count),
+      items: [...data.items, {product, cantidad: count, id:id}]
+    });
+    localStorage.setItem("Cart", JSON.stringify({
+      ...data, 
+      id: id , 
+      cantidad:JSON.stringify( data.cantidad + count),
+      items: [...data.items, {product, cantidad: count, id:id}]
+    }));
+  
+  }  history.push("/cart");}
+
+
+  const getProduct = new Promise((resolve, reject) => {
+    const lista = Instrumentos.filter((detalle) => detalle.id === Number(id));
+    const lista_resuelta = lista[0];
+    resolve(lista_resuelta);
+    // reject(alert('error'))
+  });
+
+  const getProducstFromDB = async () => {
+    try {
+      const result = await getProduct;
+      setProduct(result);
+    } catch (error) {
+      alert("No podemos mostrar el producto");
+    }
+  };
+
+  useEffect(() => {
+    getProducstFromDB();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <section>
+      {product ? (
+        <>
+          <div className="contenedor_detail">
+            <div className="contenedor_imagen">
+              <img
+                src={product.Imagen}
+                alt="Guitarra"
+                className="Imagen_Detail"
+              />
+            </div>
+            <div className="div_producto">
+              <p className="Titulo_Producto">
+                {product.Marca} {product.Modelo}
+              </p>
+              <p className="Descripcion_producto">{product.descripcion}</p>
+              <p style={{ justifyContent: "center", display: "flex" }}>
+                {product.Precio}
+              </p>
+              <Counter
+                Quantity={product.Quantity}
+                onAdd={onAdd}
+                product={`${product.Marca} ${product.Modelo}`}
+              />
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>
+          <img src={Loading} alt="Cargando..." />
+          <p className="txt_cargando">Cargando</p>
+        </div>
+      )}
+    </section>
+  );
+};
+export default Detail;
---
+++ components/cart/cart.jsx
@@ -0,0 +1,87 @@
+import "./styleCart.css";
+import { useContext } from "react";
+import { Store } from "../../store";
+import { ImCross } from "react-icons/im";
+import { AiOutlineDelete } from "react-icons/ai";
+import Loading from "../assets/loading.gif";
+
+const Cart = () => {
+  const [data, setData] = useContext(Store);
+  let copyData = data;
+  
+  
+  //ELIMINA TODO EL CART
+  function deleteAll() {
+    copyData.items = [];
+    setData({
+      ...copyData,
+    });
+    setData({
+      ...copyData,
+      cantidad: data.items.reduce((acc, i) => acc + i.cantidad, 0),
+    });
+    localStorage.setItem(
+      "Cart",
+      JSON.stringify({
+        ...data,
+        cantidad: data.items.reduce((acc, i) => acc + i.cantidad, 0),
+      })
+    );
+  }
+  //ELIMINA DONDE HAGA CLICK EL USUARIO
+  function deleteItem(id) {
+    let lista_filtrada = copyData.items.filter((e) => e.id === id);
+    var idx = copyData.items.indexOf(lista_filtrada[0]);
+    console.log(idx);
+    copyData.items.splice(idx, 1);
+    setData({
+      ...copyData,
+      cantidad: data.items.reduce((acc, i) => acc + i.cantidad, 0),
+    });
+    localStorage.setItem(
+      "Cart",
+      JSON.stringify({
+        ...data,
+        id: id,
+        cantidad: data.items.reduce((acc, i) => acc + i.cantidad, 0),
+      })
+    );
+  }
+
+  return (
+    <div>
+      {copyData ? (
+        <>
+          <div className="ContenedorCarrito">
+            <h1>
+              Estás en el carrito{" "}
+              <AiOutlineDelete
+                onClick={() => {
+                  deleteAll();
+                }}
+              />
+            </h1>
+            <h2>Cantidad total: {data.cantidad}</h2>
+            {data.items.map((i) => (
+              <h3 key="producto">
+                {i.product} - {i.cantidad}{" "}
+                <ImCross
+                  onClick={() => {
+                    deleteItem(i.id);
+                  }}
+                />{" "}
+              </h3>
+            ))}
+          </div>
+        </>
+      ) : (
+        <div>
+          <img src={Loading} alt="Cargando..." />
+          <p className="txt_cargando">Cargando</p>
+        </div>
+      )}{" "}
+    </div>
+  );
+};
+
+export default Cart;
