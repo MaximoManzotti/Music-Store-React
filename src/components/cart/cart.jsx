627ccf7f0b7cb1bb82baea1a66444cdb04f769ff
Maximo <maximo1611@gmail.com>
2021-01-21 16:32:42 -0300

    funcion nueva cart

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -9,21 +9,22 @@ import { useHistory } from "react-router-dom";
 const Cart = () => {
   const [data, setData] = useContext(Store);
   let copyData = data;
-  let precio_total = []
-  let resultado_total = 0
+  let precio_total = [];
+  let resultado_total = 0;
   const history = useHistory();
-  
+
   function PrecioTotal() {
-    data.items.map((i) => (
-    precio_total.push(i.Precio * i.cantidad)
-    ))
-     resultado_total = Number(precio_total.reduce((acc, item) => acc += item, 0))
-  setData({
-    ...data,
-    total:resultado_total
-  })
-  } PrecioTotal()
+    data.items.map((i) => precio_total.push(i.Precio * i.cantidad));
+    resultado_total =
+     Number( precio_total.reduce((acc, item) => (acc += item), 0)
+    )
+
+    Resultado(resultado_total)
+  }
+  PrecioTotal();
+
   //ELIMINA TODO EL CART
+
   function deleteAll() {
     copyData.items = [];
     setData({
@@ -39,11 +40,13 @@ const Cart = () => {
       JSON.stringify({
         ...data,
         cantidad: data.items.reduce((acc, i) => acc + i.cantidad, 0),
-        precio :  data.items.Precio,
+        precio: data.items.Precio,
       })
     );
   }
+
   //ELIMINA DONDE HAGA CLICK EL USUARIO
+
   function deleteItem(id) {
     let lista_filtrada = copyData.items.filter((e) => e.id === id);
     var idx = copyData.items.indexOf(lista_filtrada[0]);
@@ -52,7 +55,7 @@ const Cart = () => {
     setData({
       ...copyData,
       cantidad: data.items.reduce((acc, i) => acc + i.cantidad, 0),
-      precio:  data.items.Precio,    
+      precio: data.items.Precio,
     });
     localStorage.setItem(
       "Cart",
@@ -64,6 +67,16 @@ const Cart = () => {
       })
     );
   }
+
+function Resultado(resultado) {
+  setData({
+    ...data,
+    total: resultado
+    })
+}
+
+
+console.log(data)
   return (
     <div>
       {copyData ? (
@@ -75,11 +88,14 @@ const Cart = () => {
                 onClick={() => {
                   deleteAll();
                 }}
-                style={{position:'absolute', marginLeft: '0.5em'}}/>
+                style={{ position: "absolute", marginLeft: "0.5em" }}
+              />
             </h1>
             <div className="Precio_Cantidad">
-            <h3 className='Precio_total'>Precio total: ${resultado_total}</h3>
-            <h3 className='Cantidad_total'>Cantidad total: {data.cantidad}</h3>
+              <h3 className="Precio_total">Precio total: ${resultado_total}</h3>
+              <h3 className="Cantidad_total">
+                Cantidad total: {data.cantidad}
+              </h3>
             </div>
             {data.items.map((i) => (
               <h4 key="producto">
@@ -88,10 +104,15 @@ const Cart = () => {
                   onClick={() => {
                     deleteItem(i.id);
                   }}
-                style={{position:'absolute', marginLeft: '1em'}}/>
+                  style={{ position: "absolute", marginLeft: "1em" }}
+                />
               </h4>
             ))}
-            <button onClick={()=>{ history.push("/checkout")}}>
+            <button
+              onClick={() => {
+                history.push("/checkout");
+              }}
+            >
               Pasar a Comprar
             </button>
           </div>
