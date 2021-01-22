3ec234294f5b1eaf4a72a8e286c06fd84d17f486
Maximo <maximo1611@gmail.com>
2021-01-22 05:31:05 -0300

    formulario proceso

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,45 +1,50 @@
 import './Checkout_Style.css'
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Store } from "../../store/index";
 
 
 function Checkout() {
     // eslint-disable-next-line no-unused-vars
     const [data, setData] = useContext(Store);
+    const [form , setForm] = useState({
+nombre:'',
+apellido:'',
+email:'',
+telefono:'',
+tarjeta:'',
+expiracion:'',
+codigo_seguridad:''
+    })
+const handleChangeInput = (e) =>{ 
+    setForm({
+      ...form , [e.target.nombre]: e.target.value  
+     } )
+  }
+  console.log(form)
 
-    console.log(data)
 
+    
     return(
 <form className="Formulario">
-  <label>
-    Nombre:
-    <input type="text" name="name" />
-  </label>
-  <label>
-    Apellido:
-    <input type="text" name="name" />
-  </label>
-  <label>
-    Email:
-    <input type="text" name="name" />
-  </label>
-  <label>
-    Confirmar Email:
-    <input type="text" name="name" />
-  </label>
-  <label>
-    Tarjeta:
-    <input type="text" name="name" />
-  </label>
-  <label>
-    Fecha de Vencimiento:
-    <input type="text" name="name" />
-  </label>
-  <label>
-    Codigo de seguridad:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
+
+
+
+
+    <input type="text" value={form.nombre} name="Nombre" placeholder="Nombre" onChange={handleChangeInput}/>
+ 
+    <input type="text" value={form.apellido} name="Apellido" placeholder="Apellido"  onChange={handleChangeInput}/>
+
+    <input type="text" value={form.email} name="Email" placeholder="Email"  onChange={handleChangeInput}/>
+ 
+    <input type="text" value={form.telefono} name="Telefono" placeholder="Telefono"  onChange={handleChangeInput}/>
+
+    <input type="text" value={form.tarjeta} name="Tarjeta" placeholder="Tarjeta"  onChange={handleChangeInput}/>
+
+    <input type="text" value={form.expiracion} name="FechaExpiracion" placeholder="Fecha de Expiracion"  onChange={handleChangeInput}/>
+
+    <input type="text" value={form.codigo_seguridad} name="CodigoSeguridad" placeholder="Codigo de Seguridad"  onChange={handleChangeInput} />
+
+  <button>Pagar</button>
 </form>
     )
 }
